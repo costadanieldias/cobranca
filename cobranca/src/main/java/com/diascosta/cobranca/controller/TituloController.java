33ca7d2d09e0f26a4e4c8e9b818ea548353c17ce
daniel <costadanieldias@gmail.com>
2017-10-06 12:22:22 -0300

    Manter valores no form e fazer redirect.

--- cobranca/src/main/java/com/diascosta/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/diascosta/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.diascosta.cobranca.model.StatusTitulo;
 import com.diascosta.cobranca.model.Titulo;
@@ -19,6 +20,8 @@ import com.diascosta.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	private static final String REDIRECT_TITULOS_NOVO = "redirect:/titulos/novo";
 	
 	@Autowired
 	private Titulos titulos;
@@ -31,14 +34,13 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {				
-		ModelAndView mv = new ModelAndView("CadastroTitulo");		
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {						
 		if(errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}		
 		titulos.save(titulo);						
-		mv.addObject("mensagem", "Título salvo com sucesso!");		
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");		
+		return REDIRECT_TITULOS_NOVO;
 	}
 	
 	@RequestMapping
